61a29d9c2166dbb4091637561076eba0254021f9
Grad35 <aegradov@gmail.com>
2026-09-18 17:49:28 +0300

    c#

---
+++ 1.cs
@@ -0,0 +1,41 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите первое число: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите операцию (+, -, *, /): ");
+        char op = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+
+        Console.Write("Введите второе число: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        double result = op switch
+        {
+            '+' => a + b,
+            '-' => a - b,
+            '*' => a * b,
+            '/' => b != 0 ? a / b : double.NaN,
+            _ => double.NaN
+        };
+
+        Console.WriteLine($"Результат: {result}");
+    }
+
+
+
+    private void listbox1_SelectedIndexChanged(object sender, EventArgs e)
+
+
+    {
+
+
+        
+
+
+    }
+}
