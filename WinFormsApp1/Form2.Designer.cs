1ef8061038a7576934b940b65e503434e212303c
Heonanzel <tropin.iegor@yandex.ru>
2026-09-22 15:07:24 +0300

    Programm

---
+++ WinFormsApp1/Form2.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            label1 = new Label();
+            button5 = new Button();
+            button6 = new Button();
+            button8 = new Button();
+            button7 = new Button();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(63, 40);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Вопрос 1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(248, 40);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 1;
+            button2.Text = "Вопрос 2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(451, 40);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 2;
+            button3.Text = "Вопрос 3";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(623, 40);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 3;
+            button4.Text = "Вопрос 4";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(294, 185);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 15);
+            label1.TabIndex = 4;
+            // 
+            // button5
+            // 
+            button5.Enabled = false;
+            button5.Location = new Point(12, 342);
+            button5.Name = "button5";
+            button5.Size = new Size(166, 23);
+            button5.TabIndex = 5;
+            button5.Text = "Ответ 1";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Enabled = false;
+            button6.Location = new Point(225, 342);
+            button6.Name = "button6";
+            button6.Size = new Size(166, 23);
+            button6.TabIndex = 6;
+            button6.Text = "Ответ 2";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button8
+            // 
+            button8.Enabled = false;
+            button8.Location = new Point(623, 342);
+            button8.Name = "button8";
+            button8.Size = new Size(166, 23);
+            button8.TabIndex = 7;
+            button8.Text = "Ответ 4";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button7_Click;
+            // 
+            // button7
+            // 
+            button7.Enabled = false;
+            button7.Location = new Point(436, 342);
+            button7.Name = "button7";
+            button7.Size = new Size(166, 23);
+            button7.TabIndex = 8;
+            button7.Text = "Ответ 3";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button8_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(604, 116);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(184, 154);
+            pictureBox1.TabIndex = 9;
+            pictureBox1.TabStop = false;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox1);
+            Controls.Add(button7);
+            Controls.Add(button8);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(label1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form2";
+            Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Label label1;
+        private Button button5;
+        private Button button6;
+        private Button button8;
+        private Button button7;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ WinFormsApp1/Form2.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design.Serialization;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        public static int  answer = 0;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        public void Disable()
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+        }
+        public void Enable()
+        {
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+        }
+        public void Enable2()
+        {
+            button5.Enabled = true;
+            button6.Enabled = true;
+            button7.Enabled = true;
+            button8.Enabled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Какой минерал изображён на картинке?";
+            button5.Text = "Кальцит";
+            button6.Text = "Флюорит";
+            button8.Text = "Кварц";
+            button7.Text = "Сера";
+            answer = 2;
+            pictureBox1.Image = Resource1.Mineral1;
+            Disable();
+            Enable2();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Что такое Галит?";
+            button5.Text = "Поваренная соль";
+            button6.Text = "Известняк";
+            button8.Text = "Пищевая сода";
+            button7.Text = "Мел";
+            answer = 1;
+            Disable();
+            Enable2();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Какой минерал за внешнее сходство называют золотом дураков?";
+            button5.Text = "Халькопирит";
+            button6.Text = "Золотистый топаз";
+            button7.Text = "Слюда";
+            button8.Text = "Пирит";
+            answer = 4;
+            Disable();
+            Enable2();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Какой минерал самый распространённый в земной коре?";
+            button5.Text = "Полевой шпат";
+            button6.Text = "Кварц";
+            button8.Text = "Гипс";
+            button7.Text = "Кальцит";
+            answer = 2;
+            Disable();
+            Enable2();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            if (answer == 1)
+            {
+                label1.Text = "Правильно!";
+                Enable();
+            }
+            else
+            {
+                label1.Text = "Неправильно";
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (answer == 2)
+            {
+                label1.Text = "Правильно!";
+                Enable();
+                pictureBox1.Image = null;
+            }
+            else
+            {
+                label1.Text = "Неправильно";
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (answer == 3)
+            {
+                label1.Text = "Правильно!";
+                Enable();
+            }
+            else
+            {
+                label1.Text = "Неправильно";
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (answer == 4)
+            {
+                label1.Text = "Правильно!";
+                Enable();
+            }
+            else
+            {
+                label1.Text = "Неправильно";
+            }
+        }
+    }
+}
