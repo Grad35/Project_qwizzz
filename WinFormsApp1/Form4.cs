afd8fc33517faaa73e1f66e9d503b526748918fa
Ruffi0o <ibragimovt039@gmail.com>
2026-09-23 15:59:18 +0300

    dfdf

---
+++ WinFormsApp1/Form4.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Windows.Forms;
+
+namespace MyGitHubProject // Замените на namespace вашего проекта
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            // Этот метод инициализирует компоненты формы (кнопки, текст и т.д.)
+            InitializeComponent();
+        }
+    }
+}
